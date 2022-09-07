22571515a375c0a335361b60846b5c530c9d45df
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2022-09-07 13:59:06 +0200

    Create Footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+	return (
+		<div className='flex justify-center gap-5 py-3'>
+			<a href='https://www.instagram.com/erdelyiroland99/'>
+				<i className='fa-brands fa-instagram duration-300 hover:opacity-30 cursor-pointer'></i>
+			</a>
+			<a href='https://github.com/erdaboss99/Nextjs-Firebase-Todo'>
+				<i className='fa-brands fa-github-alt duration-300 hover:opacity-30 cursor-pointer'></i>
+			</a>
+		</div>
+	);
+}
+
+export default Footer;
