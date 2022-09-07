0c78c10a8979b8fa94df3187f9f5b5e9833a5a78
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2022-09-07 23:38:58 +0200

    Change styles in TodoCard to make it responsive

--- components/TodoCard.jsx
+++ components/TodoCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function TodoCard(props) {
 	const { children } = props;
 	return (
-		<div className='p-2 border flex items-strech border-white border-solid '>
+		<div className='p-2 relative sm:p-3 border flex items-stretch border-white border-solid '>
 			<div className='flex-1 flex'>{children}</div>
 			<div className='flex items-center'>
 				<i className='fa-solid fa-pencil px-2 duration-300 hover:rotate-45 cursor-pointer'></i>
